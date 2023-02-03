d8bdd038478fd34834703bff657fd4e2cb75c5f1
lorenzoajt <lorenzoajt@gmail.com>
2023-02-02 21:48:50 -0600

    fix env bug and add trip and expense to admin

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 import environ
 
+env = environ.Env()
+environ.Env.read_env()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
